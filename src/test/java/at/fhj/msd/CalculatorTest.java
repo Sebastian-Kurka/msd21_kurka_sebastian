f6c0a49631dc9e4b4fc45f6b0d61a544086fd131
Sebastian Kurka <sebastian.kurka@edu.fh-joanneum.at>
2022-10-28 18:25:29 +0200

    edited Calculator.java added proper factorial method.

--- src/test/java/at/fhj/msd/CalculatorTest.java
+++ src/test/java/at/fhj/msd/CalculatorTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Assertions;
 
-//import org.junit.jupiter.api.Before;
 
 public class CalculatorTest {
     private Calculator calc;
